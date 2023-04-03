53c312382ec0207d3f38196ceddf479b6b317cb8
Mykhailo Vovchok <mivvaw@gmail.com>
2023-04-03 14:52:54 +0300

    models changed relations

--- list/models.py
+++ list/models.py
@@ -14,7 +14,7 @@ class Task(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     progress = models.BooleanField(default=False)
-    tag = models.ForeignKey(to=Tag, on_delete=models.CASCADE, related_name="tasks")
+    tag = models.ManyToManyField(to=Tag, related_name="tasks")
 
     def __str__(self):
         return self.content
