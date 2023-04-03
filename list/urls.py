c5200c4e82088e67bb4b2886d54239dd5f574a14
Mykhailo Vovchok <mivvaw@gmail.com>
2023-04-03 13:54:44 +0300

    add urls

--- list/urls.py
+++ list/urls.py
@@ -1,9 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
-from list.views import TaskListView
+from list.views import TaskListView, TagsListView, TagsCreateView, TagUpdateView, TagDeleteView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
-]
+    path("tags/", TagsListView.as_view(), name="tag-list"),
+    path("tags/create/", TagsCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = "list"
