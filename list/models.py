15d1cc04980fb5f03bd0d93f4fe71961017e1c55
Mykhailo Vovchok <mivvaw@gmail.com>
2023-04-03 11:29:19 +0300

    add models Task and Tag

--- list/models.py
+++ list/models.py
@@ -1,3 +1,22 @@
+import datetime as datetime
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField(max_length=511)
+    datetime = models.DateTimeField(auto_now_add=True)
+    #deadline = models.DateTimeField(auto_now_add=True, optional=True)
+    progress = models.BooleanField(default=False)
+    tag = models.ForeignKey(to=Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+
